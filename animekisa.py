911820fcceba743204a47e9ad14260c1c60c40bd
Amos Nimos <76786560+AmosNimos@users.noreply.github.com>
2021-02-18 19:10:19 -0500

    Update animekisa.py

--- animekisa.py
+++ animekisa.py
@@ -6,19 +6,23 @@ import os
 import webbrowser
 import dmenu
 import sys
+import wget
 
 #----------------------------------------------------------------------------------#
 # ARGUMENTS                                                                        #
 #----------------------------------------------------------------------------------#
 pageSelect=False
 episodeSelect=False;
+download=False;
 if(len(sys.argv)>0):
 	for i in range(len(sys.argv)):
 		arg = str(sys.argv[i])
 		if arg == "-p":
-			pageSelect=True;
+			pageSelect=True
 		if arg == "-e":
-			episodeSelect=True;
+			episodeSelect=True
+		if arg == "-d":
+			download=True
 
 #----------------------------------------------------------------------------------#
 # FUNCTIONS                                                                        #
@@ -45,7 +49,7 @@ if pageSelect == True:
 	less="<"
 	while loop==True:
 		options=genList(menuSize,pagesIndex,less,more)
-		choice = dmenu.show(options)
+		choice = dmenu.show(options, prompt='select page')
 		if str(choice) == less:
 			pagesIndex-=menuSize
 		elif str(choice) == more:
@@ -82,7 +86,7 @@ for index in animes:
 #----------------------------------------------------------------------------------#
 # OPTIONS                                                                          #
 #----------------------------------------------------------------------------------#
-choice = dmenu.show(names)
+choice = dmenu.show(names, prompt='select anime')
 
 #----------------------------------------------------------------------------------#
 # SCAN                                                                             #
@@ -113,7 +117,7 @@ if episodeSelect == True:
 	(menuSize,pagesIndex,less,more)
 	firstEpisode = 1
 	options=genList(int(episode[0]),firstEpisode,None,None)
-	selectedEpisode = dmenu.show(options)
+	selectedEpisode = dmenu.show(options, prompt='select episode')
 
 ## scan anime kisa for download link
 url="https://animekisa.tv/"+str(choice)+"-episode-"+str(selectedEpisode)
@@ -129,26 +133,55 @@ jss = str(js[index]).split(';')
 for lines in jss:
 	if "var VidStreaming = " in lines:
 		VidStreaming = str(lines).split('"')
-print(VidStreaming)
 
 ## scan download page for download
 # example "https://gogo-play.net/load.php?id=MTUyNTE4&title=Beastars+2nd+Season&typesub=SUB&sub=&cover=Y292ZXIvYmVhc3RhcnMtMm5kLXNlYXNvbi5wbmc="
-url=str(VidStreaming[1])
-webbrowser.open(str(url))
 
-##to download
-# html_content = requests.get(url).text
-# downPage = BeautifulSoup(html_content, "lxml")
-# js = downPage.find_all('script')
-# links = []
-# for index in js:
-# 	if "https://gogo-play.net/download?id=" in str(index):
-# 		links=str(index)
-# links = links.split('"')
-# for index in links:
-# 	if "https://gogo-play.net/download?id=" in str(index):
-# 		downLink=str(index)
-# webbrowser.open(str(downLink))
+url=str(VidStreaming[1])
+if download==True:
+	##to download
+	html_content = requests.get(url).text
+	downPage = BeautifulSoup(html_content, "lxml")
+	js = downPage.find_all('script')
+	links = []
+	for index in js:
+		if "https://gogo-play.net/download?id=" in str(index):
+			links=str(index)
+	links = links.split('"')
+	for index in links:
+		if "https://gogo-play.net/download?id=" in str(index):
+			url=str(index)
+	#to download video directly
+	html_content = requests.get(url).text
+	downVideo = BeautifulSoup(html_content, "lxml")
+	url=""
+	loop=0
+	finalRes=""
+	res=["(360P - mp4)","(480P - mp4)","(720P - mp4)","(1080P - mp4)","(HDP - mp4)"]
+	while url=="" and loop<len(res):
+		for index in downVideo.find_all('a', href=True):
+			text = str(index.get_text())
+			#print(text[-12:])
+			if text[-12:] == res[loop]:
+				url = index['href']
+				finalRes=res[loop]
+		loop+=1
+	if url == "":
+		print("no download available.")
+		exit()
+	#finalRes = str(finalRes)[:4]
+	#finalRes = str(finalRes)[-3:]
+	#videoName=str(choice)+'-episode-'+str(selectedEpisode)+'-'+finalRes+'p.mp4'
+	#videoFile='~/anime/'
+	#download_file(url,videoFile+videoName)
+	#print("downloading"+videoName+"please wait")
+	#myfile = requests.get(url)
+	#open(videoFile+videoName+'.mp4', 'wb').write(myfile.content)
+	#wget.download(url, videoFile+videoName+'.mp4')
+	# example https://gogo-play.net/download?id=MTUwMzA3&title=Tatoeba+Last+Dungeon+Mae+no+Mura+no+Shounen+ga+Joban+no+Machi+de+Kurasu+Youna+Monogatari&typesub=SUB
+	webbrowser.open(str(url))
+
+## scan downlink for actual download link
 
 
 
